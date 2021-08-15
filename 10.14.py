7c5f3e369478df784b9a13ed0d670c02d72ddf80
MarcoMorato <87669918+MarcoMorato@users.noreply.github.com>
2021-08-15 11:34:53 +0300

    сохраняю перед изменением

---
+++ 10.14.py
@@ -0,0 +1,28 @@
+import cellphone
+
+
+def main():
+    phone = make_list()
+    display_list(phone)
+
+
+def make_list():
+    phone_list = []
+    for count in range(1, 3):
+        print('Номер телефона', count,':')
+        man = input('Производитель ')
+        mod = input('Модель ')
+        retail = float(input('Цена '))
+        phone = cellphone.CellPhone(man, mod, retail)
+        phone_list.append(phone)
+    return phone_list
+
+
+def display_list(list):
+    for x in list:
+        print(x.get_manufact())
+        print(x.get_model())
+        print(x.get_price())
+
+
+main()
